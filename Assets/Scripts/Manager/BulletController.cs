df7edff816c8409d12ab4164dc350f36e2d75844
OwOmm <gkdldy01@gmail.com>
2023-11-02 07:45:25 +0900

    231102 Add Boss First Phase

---
+++ Assets/Scripts/Manager/BulletController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    Rigidbody rb = null;
+    public float speed = 200f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void Init(Quaternion _transform)
+    {
+        transform.rotation = _transform;
+    }
+    private void Start()
+    {
+        rb.velocity = transform.forward;
+    }
+}
