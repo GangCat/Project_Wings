2a0e73f8bf7547be490e261b012a8eb255255f4d
PastleBlue <>
2023-11-12 16:48:25 +0900

    Water Effect

---
+++ Assets/Scripts/WaterEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEffect : MonoBehaviour
+{
+    private GameObject Effect;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ãæµ¹ ´ð´Â´ñÁÒ");
+    }
+}
