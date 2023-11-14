1db32f62c833597ba6950920e2caaeb2906f5608
GangCat <windy9300@gmail.com>
2023-11-14 14:41:01 +0900

    버그수정

--- Assets/Scripts/ActionCamera.cs
+++ Assets/Scripts/ActionCamera.cs
@@ -15,6 +15,9 @@ public class ActionCamera : MonoBehaviour
     public void StartAction(int _curPhaseNum)
     {
         cam.enabled = true;
+
+        SetStartPosAndRot(_curPhaseNum);
+
         anim.SetTrigger($"Phase{_curPhaseNum}");
         StartCoroutine(CheckIsAnimFinishCoroutine(_curPhaseNum));
 
@@ -22,6 +25,40 @@ public class ActionCamera : MonoBehaviour
             isLastAction = true;
 
         Debug.Log(_curPhaseNum);
+    }
+
+    private void SetStartPosAndRot(int _curPhaseNum)
+    {
+        if(_curPhaseNum == 0)
+        {
+            SetPos(230, 539, -405);
+            SetRot(10.902f, -80, 0f);
+        }
+        else if(_curPhaseNum == 1)
+        {
+            SetPos(-594, 190, -862);
+            SetRot(10.902f, 34.582f, 0);
+        }
+        else if(_curPhaseNum == 2)
+        {
+            SetPos(-4.3f, 122f, -162.6f);
+            SetRot(-45f, 0f, 0f);
+        }
+        else if(_curPhaseNum >= 3)
+        {
+            SetPos(0f, 597f, -972f);
+            SetRot(14.354f, 0f, 0f);
+        }
+    }
+
+    private void SetPos(float _x, float _y, float _z)
+    {
+        transform.position = new Vector3(_x, _y, _z);
+    }
+
+    private void SetRot(float _x, float _y, float _z)
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(_x, _y, _z));
     }
 
     private IEnumerator CheckIsAnimFinishCoroutine(int _curPhaseNum)
