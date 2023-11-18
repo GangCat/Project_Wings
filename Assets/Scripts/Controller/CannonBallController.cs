e9586faa95457811d9f9493e6c8ea1836bc002bc
OwOmm <gkdldy01@gmail.com>
2023-11-19 01:24:26 +0900

    dd

--- Assets/Scripts/Controller/CannonBallController.cs
+++ Assets/Scripts/Controller/CannonBallController.cs
@@ -1,6 +1,7 @@
 using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
@@ -68,12 +69,20 @@ public class CannonBallController : AttackableObject, ISubscriber
             soundManager.PlayAudio(GetComponent<AudioSource>(), (int)SoundManager.ESounds.CANNONCRUSHSOUND);
         }
 
-
+        
         if (_other.gameObject.layer == LayerMask.NameToLayer("BossBody"))
+        {
+            SetObjectToInvisible();
+            Invoke("SetObjectToVisible", 5f);
             memoryPool.DeactivateCannonBall(gameObject);
+        }
 
         if (AttackDmg(_other))
+        {
+            SetObjectToInvisible();
+            Invoke("SetObjectToVisible", 5f);
             memoryPool.DeactivateCannonBall(gameObject);
+        }
         //물인지 그 외의 물체와 충돌인지 검사 > 플레이어와의 거리계산 > 물에서 터지는 포탄 소리 or 그냥 폭발소리중 알맞는 소리 삽입
     }
 
@@ -92,5 +101,15 @@ public class CannonBallController : AttackableObject, ISubscriber
     {
         if (_message == EMessageType.PHASE_CHANGE)
             isPhaseChanged = true;
+    }
+    private void SetObjectToInvisible()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<Collider>().enabled = false;
+    }
+    private void SetObjectToVisible()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        gameObject.GetComponent<Collider>().enabled = true;
     }
 }
